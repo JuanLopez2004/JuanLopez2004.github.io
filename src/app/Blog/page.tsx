670475bbf9377d328256869eb7377176e4b7aee0
JuanLopez2004 <jlopeze22004@gmail.com>
2026-05-07 13:25:44 -0500

    refactor

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -54,7 +54,8 @@ export default function Home() {
         link1='https://web.archive.org/web/20050305132752/http://niulug.org/'
         link1Label='linux users group wayback '
         />
-		
+
+      
       </main>
 
       <Footer />
